1b8687670d9e005bc294e3c7de56e5fa476d17fd
babaHedz <hedi.rivas.pro@gmail.com>
2024-09-23 11:06:24 +0200

    header done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,34 +2,35 @@
 import Logo from "./Logo";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { cn } from "../lib/utils";
 
 export default function Header() {
   const pathname = usePathname();
 
+  const routes = [
+    { label: "Home", path: "/" },
+    { label: "Products", path: "/products" },
+  ];
+
   return (
-    <header className="border-b bg-cyan-700 border-slate-400 flex justify-between items-center px-5 py-5">
+    <header className=" bg-cyan-700 flex justify-between items-center px-5 py-5">
       <Logo />
 
       <nav>
         <ul className="flex space-x-2">
-          <li
-            className={`text-slate-200 font-semibold transition ${
-              pathname === "/"
-                ? "bg-white text-slate-600 px-2 rounded-sm font-bold"
-                : "hover:bg-slate-300 text-slate-600 px-2 rounded-sm"
-            }`}
-          >
-            <Link href="/">Home</Link>
-          </li>
-          <li
-            className={`text-slate-200 font-semibold ${
-              pathname === "/products"
-                ? "bg-white text-slate-600 px-2 rounded-sm font-bold"
-                : ""
-            }`}
-          >
-            <Link href="/products">Products</Link>
-          </li>
+          {routes.map(({ label, path }) => (
+            <li
+              key={path}
+              className={cn(
+                "rounded-sm px-2 py-1 text-white/80 transition hover:text-white focus:text-white",
+                {
+                  "bg-black/10 text-white": pathname === path,
+                }
+              )}
+            >
+              <Link href={path}>{label}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
